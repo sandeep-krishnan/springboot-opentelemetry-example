a35d1b587c1137e31d2da50f0684d9cfc6c2fc8d
Sandeep Krishnan <sandeep@Sandeeps-MacBook-Air.local>
2023-10-25 22:49:40 +0530

    initial commit

---
+++ cart-service/src/main/java/dev/codemesh/opentelemetry/cart/EcommerceCartServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.codemesh.opentelemetry.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCartServiceApplication.class, args);
+	}
+
+}
---
+++ cart-service/src/test/java/dev/codemesh/opentelemetry/cart/EcommerceCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codemesh.opentelemetry.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
